755f5c3d7cf2d158d971db309699d5270178a236
YitingLiu97 <yl3466@nyu.edu>
2023-01-14 16:26:45 -0500

    skybox and controllers

--- Project/js/skybox.js
+++ Project/js/skybox.js
@@ -1,10 +1,16 @@
 WL.registerComponent('skybox', {
-      skyMat: {type: WL.Type.Material}
+    skyMat: {type: WL.Type.Material},
+    light1: {type: WL.Type.Object},
+    light2: {type: WL.Type.Object}
+
+
 }, {
     init: function() {
        // console.log('init() with param', this.param);
     },
     start: function() {
+       this.lightComponent  = this.light1.getComponent('light');
+       this.lightComponent2  = this.light2.getComponent('light');
 
        // console.log('start() with param', this.param);
         this.target = this.object.getComponent('cursor-target') || this.object.addComponent('cursor-target');
@@ -23,5 +29,10 @@ WL.registerComponent('skybox', {
         this.skyMat.colorStop2=[e,d,c,1];
         this.skyMat.colorStop1=[e,d,c,1];
         this.skyMat.colorStop0=[1,1,1,1];
+
+        this.lightComponent.color.set([e,d,c]);
+        this.lightComponent2.color.set([e,d,c]);
+
+
     }
 });
