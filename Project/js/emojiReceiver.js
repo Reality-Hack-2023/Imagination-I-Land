bcf965bc32cc22f2ee234019e52a68ec213fe23a
YitingLiu97 <yl3466@nyu.edu>
2023-01-14 20:11:59 -0500

    edit scene for changing colors

---
+++ Project/js/emojiReceiver.js
@@ -0,0 +1,30 @@
+WL.registerComponent('emojiReceiver', {
+    peerComponent: {type: WL.Type.Object},
+    emojiSpawner: {type:WL.Type.Object},
+}, {
+    init: function() {
+        console.log('init() with param', this.param);
+    },
+    start: function() {
+        let peerManager=  this.peerComponent.getComponent('peer-manager');
+        let emoSpawner=  this.peerComponent.emojiSpawner('emoji_particle_shooter');
+        
+        // send message when you are interacting in XR 
+        //peerManager.sendPackage("emoji","heart");
+        
+        peerManager.addNetworkDataRecievedCallback("emoji",()=>{
+            console.log("data received, needs to spawn");
+            emoSpawner.spawn();
+        });  
+        
+        peerManager.removeNetworkDataRecievedCallback("emoji",()=>{
+            console.log("data received, needs to REMOVE");
+        });
+        
+        
+        console.log('start() with param', this.param);
+    },
+    update: function(dt) {
+        console.log('update() with delta time', dt);
+    },
+});
